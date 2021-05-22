72611abf3a4efbe01c797d711dd3d2c9e8e0396d
eraldoplimajr <eraldoplimajr@gmail.com>
2021-05-22 17:43:30 -0300

    Inserindo registros com merge

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -16,6 +16,25 @@ import com.algaworks.ecommerce.model.Produto;
 public class OperacoesComTransacaoTest extends EntityManagerTest{
 	
 	@Test
+	public void inserirObjetoComMerge() {
+		
+		Produto produto = new Produto();
+		produto.setId(4);
+		produto.setNome("Microfone Rode Videmic");
+		produto.setDescicao("A melhor qualidade de som");
+		produto.setPreco(new BigDecimal(1000));
+		
+		entityManager.getTransaction().begin();
+		entityManager.merge(produto);
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+		Assert.assertNotNull(produtoVerificacao);		
+	}
+	
+	@Test
 	public void atualizarObjetoGerenciado() {
 		Produto produto = entityManager.find(Produto.class, 1);
 		
