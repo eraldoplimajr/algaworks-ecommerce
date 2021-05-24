2fb2855193fff6369d98cbada237a045cfcff101
eraldoplimajr <eraldoplimajr@gmail.com>
2021-05-23 23:28:48 -0300

    Configurando a geração de identificador com a strategy Sequence.

--- src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.EqualsAndHashCode;
@@ -25,7 +26,8 @@ public class Categoria {
 	
 	@EqualsAndHashCode.Include
 	@Id
-	@GeneratedValue (strategy = GenerationType.AUTO)
+	@GeneratedValue (strategy = GenerationType.SEQUENCE, generator = "seq")
+	@SequenceGenerator(name = "seq", sequenceName = "sequencia_chave_primaria")
 	private Integer id;
 	
 	private String nome;
