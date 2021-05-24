ed406e1b1ce216506703f3eba06e77013450c5be
eraldoplimajr <eraldoplimajr@gmail.com>
2021-05-23 22:33:49 -0300

    Mapeando objetos embutidos com embeddable

--- src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -43,4 +44,7 @@ public class Pedido {
 	
 	@Enumerated(EnumType.STRING)
 	private StatusPedido status;
+	
+	@Embedded
+	private EnderecoEntregaPedido enderecoEntrega;
 }
