a769339e6eb805fbe10ee483941b6c9df05661da
eraldoplimajr <eraldoplimajr@gmail.com>
2021-05-27 00:05:38 -0300

    Verificando comportamento Eager e Lazy

---
+++ src/test/java/com/algaworks/ecommerce/relacionamentos/EagerELazyTest.java
@@ -0,0 +1,22 @@
+package com.algaworks.ecommerce.relacionamentos;
+
+import org.junit.Test;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
+
+/**
+ *
+ * @author Eraldo Lima
+ *
+ */
+public class EagerELazyTest extends EntityManagerTest{
+	
+	@Test
+	public void verificarComportamento() {
+		Pedido pedido = entityManager.find(Pedido.class, 1);
+		
+		pedido.getListaItemPedido().isEmpty();
+	}
+
+}
