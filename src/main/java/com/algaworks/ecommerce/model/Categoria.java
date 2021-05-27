d6714dfc099805cd14a3304171db55ae8ba1a55f
eraldoplimajr <eraldoplimajr@gmail.com>
2021-05-26 22:52:34 -0300

    Mapeando relacionamento ManyToMany

--- src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -40,4 +41,7 @@ public class Categoria {
 	
 	@OneToMany(mappedBy = "categoriaPai")
 	private List<Categoria> listaCategorias;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos;
 }
