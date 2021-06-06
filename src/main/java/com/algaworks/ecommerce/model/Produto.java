f2abfdcc8004bb614c1340eaa296246c61626a03
eraldoplimajr <eraldoplimajr@gmail.com>
2021-06-06 04:32:08 -0300

    Inserindo coleções de tipos básicos com @ElementCollection

--- src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -4,7 +4,9 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
@@ -57,5 +59,11 @@ public class Produto {
 	private List<Categoria> categorias;
 	
 	@OneToOne(mappedBy = "produto")
-	private Estoque estoque;	
+	private Estoque estoque;
+	
+	@ElementCollection
+	@CollectionTable(name = "produto_tag",
+					 joinColumns = @JoinColumn(name = "produto_id"))
+	@Column(name = "tag")
+	private List<String> tags;
 }
