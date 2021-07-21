14ec6b9531fc015862a39331655f4a930b2aa438
eraldoplimajr <eraldoplimajr@gmail.com>
2021-07-21 02:00:46 -0300

    Herança com single table

--- src/main/java/com/algaworks/ecommerce/model/PagamentoCartao.java
+++ src/main/java/com/algaworks/ecommerce/model/PagamentoCartao.java
@@ -1,8 +1,8 @@
 package com.algaworks.ecommerce.model;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -14,8 +14,9 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@DiscriminatorValue("cartao")
 @Entity
-@Table(name = "pagamento_cartao")
+//@Table(name = "pagamento_cartao")
 public class PagamentoCartao extends Pagamento {
 	
 	@Column(name = "numero_cartao")
