516c146e14ec639801c1a1cd1e3d37e2034fa4a9
eraldoplimajr <eraldoplimajr@gmail.com>
2021-07-22 19:54:57 -0300

    Exercício reconfigurar single table

--- src/main/java/com/algaworks/ecommerce/model/PagamentoCartao.java
+++ src/main/java/com/algaworks/ecommerce/model/PagamentoCartao.java
@@ -3,7 +3,6 @@ package com.algaworks.ecommerce.model;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -17,7 +16,7 @@ import lombok.Setter;
 @Setter
 @DiscriminatorValue("cartao")
 @Entity
-@Table(name = "pagamento_cartao")
+//@Table(name = "pagamento_cartao")
 public class PagamentoCartao extends Pagamento {
 	
 	@Column(name = "numero_cartao")
