ebe3f726fc83979456096bd8ac0117b985002fc7
eraldoplimajr <eraldoplimajr@gmail.com>
2021-07-24 17:59:09 -0300

    Adicionando unique constraints e indexe a tabela cliente

---
+++ src/test/java/com/algaworks/ecommerce/ddl/DDLTest.java
@@ -0,0 +1,19 @@
+package com.algaworks.ecommerce.ddl;
+
+import org.junit.Test;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+
+/**
+ *
+ * @author Eraldo Lima
+ *
+ */
+public class DDLTest extends EntityManagerTest{
+	
+	@Test
+	public void gerarDDL() {
+		// Teste vazio... Somente para gerar DDL		
+	}
+
+}
