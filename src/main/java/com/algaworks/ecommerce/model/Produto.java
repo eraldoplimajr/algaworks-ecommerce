52118fa6ace4edc2479ed76a3339bb7b13e43460
eraldoplimajr <eraldoplimajr@gmail.com>
2021-07-24 18:25:42 -0300

    Execício adicionando constraints e index nas tabelas produto, categoria

--- src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -9,11 +9,14 @@ import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import com.algaworks.ecommerce.listener.GenericoListener;
 
@@ -29,6 +32,8 @@ import lombok.Setter;
 @Setter
 @EntityListeners(GenericoListener.class)
 @Entity
+@Table(name = "produto", uniqueConstraints = @UniqueConstraint(name = "unq_nome", columnNames = { "nome" }),
+						 indexes = @Index(name = "idx_nome", columnList = "nome"))
 public class Produto extends EntidadeBaseInteger {
 		
 	private String nome;
