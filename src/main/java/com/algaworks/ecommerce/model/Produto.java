39a0788755ee821331eb31c090ff3b39118674d3
eraldoplimajr <eraldoplimajr@gmail.com>
2021-07-25 00:22:56 -0300

    Exercício adicionando foreignKey a outras entidades

--- src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -9,6 +9,7 @@ import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.ForeignKey;
 import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -55,8 +56,8 @@ public class Produto extends EntidadeBaseInteger {
 	
 	@ManyToMany
 	@JoinTable(name = "produto_categoria", 
-			   joinColumns = @JoinColumn(name = "produto_id"),
-			   inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+			   joinColumns = @JoinColumn(name = "produto_id", foreignKey = @ForeignKey(name = "fk_produto_categoria_produto")),
+			   inverseJoinColumns = @JoinColumn(name = "categoria_id", foreignKey = @ForeignKey(name = "fk_produto_categoria_categoria")))
 	private List<Categoria> categorias;
 	
 	@OneToOne(mappedBy = "produto")
@@ -64,12 +65,12 @@ public class Produto extends EntidadeBaseInteger {
 	
 	@ElementCollection
 	@CollectionTable(name = "produto_tag",
-					 joinColumns = @JoinColumn(name = "produto_id"))
+					 joinColumns = @JoinColumn(name = "produto_id", foreignKey = @ForeignKey(name = "fk_produto_tag_produto")))
 	@Column(name = "tag", length = 50, nullable = false)
 	private List<String> tags;
 	
 	@ElementCollection
 	@CollectionTable(name = "produto_atributo",
-					 joinColumns = @JoinColumn(name = "produto_id"))
+					 joinColumns = @JoinColumn(name = "produto_id"), foreignKey = @ForeignKey(name = "fk_produto_atributo_produto"))
 	private List<Atributo> atributos;
 }
