c1e9a46e2df873048f8d40940d13953e2f3aec1d
eraldoplimajr <eraldoplimajr@gmail.com>
2021-07-25 00:06:34 -0300

    Configurando JoinColumn, name, nullable, foreignKey

--- src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
+++ src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.MapsId;
@@ -26,7 +27,7 @@ public class NotaFiscal extends EntidadeBaseInteger {
 		
 	@MapsId
 	@OneToOne(optional = false)
-	@JoinColumn(name = "pedido_id")
+	@JoinColumn(name = "pedido_id", nullable = false, foreignKey = @ForeignKey(name = "fk_nota_fiscal_pedido"))
 	private Pedido pedido;
 	
 	@Lob
