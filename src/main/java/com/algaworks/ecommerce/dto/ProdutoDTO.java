8265e8c6200745affa3416d60eba56ca5172db97
eraldoplimajr <eraldoplimajr@gmail.com>
2021-08-14 00:17:46 -0300

    Trabalhando com projeções e DTO

---
+++ src/main/java/com/algaworks/ecommerce/dto/ProdutoDTO.java
@@ -0,0 +1,23 @@
+package com.algaworks.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Eraldo Lima
+ *
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProdutoDTO {
+	
+	private Integer id;
+	
+	private String nome;
+
+}
