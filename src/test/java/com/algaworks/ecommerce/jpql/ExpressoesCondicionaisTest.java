cc8a9803fd18f0f360f71c45bca5db3c6fd6f744
eraldoplimajr <eraldoplimajr@gmail.com>
2021-08-15 23:01:03 -0300

    Usando expressão condicional like

---
+++ src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -0,0 +1,32 @@
+package com.algaworks.ecommerce.jpql;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+
+/**
+ *
+ * @author Eraldo Lima
+ *
+ */
+public class ExpressoesCondicionaisTest extends EntityManagerTest{
+	
+	@Test
+	public void usarExpressaoCondicionalLike() {
+		String jpql = "select c from Cliente c where c.nome like concat('%', :nome, '%')";
+		
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+		typedQuery.setParameter("nome", "a");
+		
+		List<Object[]> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+		
+	}
+
+}
